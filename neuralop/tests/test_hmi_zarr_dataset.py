592e25ea993b3df1441bbd7a3f987110136e8546
Marcus Hughes <hughes.jmb@gmail.com>
2023-08-16 15:57:54 -0600

    began creating time index and xarray
    Co-authored-by: Kiera van der Sande <kiera.vds@gmail.com>
    Co-authored-by: Andres Muñoz-Jaramillo <amunozj@users.noreply.github.com>

--- neuralop/tests/test_hmi_zarr_dataset.py
+++ neuralop/tests/test_hmi_zarr_dataset.py
@@ -1,6 +1,11 @@
 import pytest
 from neuralop.datasets.hmi_zarr_dataset import HMIZarrDataset
 
+def test_select_times():
+    filename = '/d0/magnetograms/hmi/preprocessed/hmi_stacks_2021_2022_96m_full.zarr'
+    dataset = HMIZarrDataset(filename,months=[1])
+    assert all([t.month==1 for t in dataset.t_obs])
+
 def test_get_item():
     print("testing item shape")
     filename = '/d0/magnetograms/hmi/preprocessed/hmi_stacks_2021_2022_96m_full.zarr'
