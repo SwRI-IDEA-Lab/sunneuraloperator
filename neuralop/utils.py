14859016aec85b2b90698b6a23468b61bb8f2ae5
Andrés Muñoz-Jaramillo <andres.munoz@swri.org>
2023-09-06 10:59:26 -0600

    Initialize train_hmi and convert paths to project root

--- neuralop/utils.py
+++ neuralop/utils.py
@@ -138,7 +138,7 @@ def set_wandb_api_key(api_key_file='../config/wandb_api_key.txt'):
             key = f.read()
         os.environ['WANDB_API_KEY'] = key.strip()
 
-def get_wandb_api_key(api_key_file='../config/wandb_api_key.txt'):
+def get_wandb_api_key(api_key_file='config/wandb_api_key.txt'):
     import os
     try:
         return os.environ['WANDB_API_KEY']
@@ -146,3 +146,5 @@ def get_wandb_api_key(api_key_file='../config/wandb_api_key.txt'):
         with open(api_key_file, 'r') as f:
             key = f.read()
         return key.strip()
+    
+
